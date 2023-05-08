d3245dcbf7b4add26b0e48ab78f7330c9ff687f3
mmadeya91 <mzm0227@auburn.edu>
2023-05-07 22:29:15 -0400

    adding questions dropdown to short story page

--- PracticeItalianApp/shortStoryView.swift
+++ PracticeItalianApp/shortStoryView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct shortStoryView: View {
     
     @State var showPopUpScreen: Bool = false
+    @State var showQuestionDropdown: Bool = false
     
     var body: some View {
         GeometryReader{ geo in
@@ -21,16 +22,21 @@ struct shortStoryView: View {
                     .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
                     .opacity(1.0)
                 
-                testStory(showPopUpScreen: self.$showPopUpScreen).frame(width: 350, height:500).background(Color.white.opacity(0.75)).cornerRadius(20).overlay( RoundedRectangle(cornerRadius: 16)
+                testStory(showPopUpScreen: self.$showPopUpScreen).frame(width: 350, height:400).background(Color.white.opacity(0.75)).cornerRadius(20).overlay( RoundedRectangle(cornerRadius: 16)
                     .stroke(.gray, lineWidth: 6))
-                .shadow(radius: 10)
+                    .shadow(radius: 10)
+                    .offset(y:-90)
                 
                 if showPopUpScreen{
-                    popUpView(showPopUpScreen: self.$showPopUpScreen)
+                    popUpView(showPopUpScreen: self.$showPopUpScreen).transition(.slide).animation(.easeIn)
                 }
                 
-
+                questionsDropDownBar(showQuestionDropdown: self.$showQuestionDropdown).position(x:200, y:525)
                 
+                if showQuestionDropdown{
+                    questionsView().position(x:200, y:655).transition(.move(edge: .bottom)).animation(.spring())
+                }
+
             }
         }
     }
@@ -86,18 +92,125 @@ struct shortStoryView: View {
                     .multilineTextAlignment(.center)
                 
             
-            }.frame(width: 300, height: 300)
+            }.frame(width: 300, height: 200)
                 .background(Color.white)
                 .cornerRadius(20)
                 .shadow(radius: 20)
+   
+        }
+    }
+    
+    struct questionsDropDownBar: View{
+        
+        @Binding var showQuestionDropdown: Bool
+        
+        var body: some View{
+            
+            VStack{
+                
+                HStack{
+                    
+                    Text("Questions")
+                        .font(Font.custom("Arial Hebrew", size: 20))
+                        .padding(.leading, 10)
+                        .padding(.top, 5)
+                    
+                    Button(action: {
+                        showQuestionDropdown.toggle()
+                    }, label: {
+                        Image(systemName: "chevron.down.square.fill")
+                            .scaleEffect(2.8)
+                    }).offset(x: 223)
+                }.frame(width: 360, height: 40, alignment: .leading)
+                    .background(Color.white)
+                    .overlay( RoundedRectangle(cornerRadius: 4)
+                        .stroke(.gray, lineWidth: 3))
+                    .shadow(radius: 10)
+            }
             
         }
     }
     
+    struct questionsView: View {
+        
+        
+        var body: some View {
+            VStack(alignment: .leading){
+                Text("Where was Cristoro Columbo born?")
+                    .font(Font.custom("Arial Hebrew", size: 18))
+                    .padding([.leading, .top], 20)
+                
+                radioButtons()
+                
+    
+            }.frame(width: 360, height: 200)
+                .background(Color.white).overlay(RoundedRectangle(cornerRadius: 5)
+                    .stroke(.gray, lineWidth: 3))
+        }
+    }
+    
+    struct radioButtons: View{
+        var body: some View{
+            VStack{
+                
+                Button(action: {}, label: {
+                    
+                    HStack{
+                        Text("test1")
+                        
+                        Spacer()
+                        
+                        Circle().fill(Color.gray.opacity(0.25)).scaleEffect(0.5)
+                    }.padding([.leading, .trailing], 20)
+                })
+                
+                Button(action: {}, label: {
+                    
+                    HStack{
+                        Text("test2")
+                        
+                        Spacer()
+                        
+                        Circle().fill(Color.gray.opacity(0.25)).scaleEffect(0.5)
+                    }.padding([.leading, .trailing], 20)
+                }).offset(y: -15)
+                
+                Button(action: {}, label: {
+                    
+                    HStack{
+                        Text("test3")
+                        
+                        Spacer()
+                        
+                        Circle().fill(Color.gray.opacity(0.25)).scaleEffect(0.5)
+                    }.padding([.leading, .trailing], 20)
+                }).offset(y: -30)
+                
+                HStack{
+                    Button(action: {}, label: {
+                        Image(systemName: "chevron.backward.to.line")
+                            .scaleEffect(1.75)
+                        
+                    }).padding(.leading, 130)
+                    
+                    Spacer()
+                    
+                    Button(action: {}, label: {
+                        Image(systemName: "chevron.forward.to.line")
+                            .scaleEffect(1.75)
+                        
+                    }).padding(.trailing, 130)
+                }.offset(y:-18)
+            }
+        }
+    }
+    
+    
+    
     struct textModifer : ViewModifier {
         func body(content: Content) -> some View {
             content
-                .font(Font.custom("Arial Hebrew", size: 20))
+                .font(Font.custom("Arial Hebrew", size: 18))
                 .padding(.top, 40)
                 .padding(.leading, 40)
                 .padding(.trailing, 40)
